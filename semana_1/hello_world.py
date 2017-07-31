1247b8002ebeaf8c6a839c64d31b884727971149
Caio Le Petit <caio.lepetit@gmail.com>
2017-07-31 16:05:20 -0300

    First commit

---
+++ semana_1/hello_world.py
@@ -0,0 +1,5 @@
+hello = "Hello"
+
+world = "World"
+
+print (hello, world)
---
+++ semana_1/operacoes.py
@@ -0,0 +1,26 @@
+a = 5
+b = 10
+
+soma = a + b
+
+subtracao = a - b
+
+multiplicacao = a * b
+
+divisao = a / b
+
+potencia = a ** b
+
+resto = a % b
+
+print ("A soma de", a, "+", b, "é", soma)
+
+print ("A subtração de", a, "-", b, "é", subtracao)
+
+print ("A multiplicação de", a, "*", b, "é", multiplicacao)
+
+print ("A divisão de", a, "/", b, "é", divisao)
+
+print ("A potência de", a, "**", b, "é", potencia)
+
+print ("O resto de", a, "/", b, "é", resto)
